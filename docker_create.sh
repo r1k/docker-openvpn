7fb18ab8f61899127514a2bad17d3664bc5166b1
r1k <3041753+r1k@users.noreply.github.com>
2019-06-30 20:14:52 +0100

    Non host mode no working config

--- docker_create.sh
+++ docker_create.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-CONTAINER_NAME="openvpn2"
+CONTAINER_NAME="openvpn"
 IMAGE_NAME="linuxserver/openvpn-as"
 
 create() {
     docker create \
         --name $CONTAINER_NAME \
-	--privileged \
-        --net=host \
+        --cap-add=NET_ADMIN \
         -e PUID=1000 \
         -e PGID=1000 \
         -e TZ=Europe/London \
-        -e INTERFACE=enp3s0 \
+        -e INTERFACE=eth0 \
+        -p 943:943 \
+        -p 9443:9443 \
+        -p 1194:1194/udp \
         -v /mnt/storage1/openvpn/config:/config \
         --restart unless-stopped \
         $IMAGE_NAME
